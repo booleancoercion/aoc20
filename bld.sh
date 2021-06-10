5361a16c545d6feb963cab122d136206a7de5a23
bool <booleancoercion@gmail.com>
2021-06-10 20:17:14 +0300

    Revamped build script and added vscode json files

--- bld.sh
+++ bld.sh
@@ -1,1 +1,4 @@
-clang src/*.c src/days/*.c libs/hashmap/*.c libs/linkedlist/*.c $(cat ./compile_flags.txt)
+files=$(find . -type f -name '*.c')
+flags=$(cat ./compile_flags.txt)
+
+clang $files $flags
